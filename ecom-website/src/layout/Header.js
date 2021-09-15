d53d839bd36d1b35036cae4823609c653c773df3
SteFowles-White <fowleswhite@gmail.com>
2021-09-15 22:24:28 +0100

    working o the navigation menu

--- ecom-website/src/layout/Header.js
+++ ecom-website/src/layout/Header.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import styles from './header.module.css'
 
 const Header = () => {
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li><NavLink to='/'>Home</NavLink></li>
-                    <li><NavLink to='/mens'>Mens</NavLink></li>
-                    <li><NavLink to='womens'>Womens</NavLink></li>
-                    <li><NavLink to='basket'>Basket</NavLink></li>
-
+        <header className='container-fluid'>
+            <nav className={`row flex-direction-row flex header ${styles.header}`}>
+                <div className='col-xs-3 flex justify-content-center align-items-aligncenter'>
+                    test text
+                </div>
+                <ul className='col-xs-6 flex-direction-row flex justify-content-center align-items-aligncenter'>
+                    <li className={`p-1 ${styles.nav__link}`}><NavLink to='/'>Home</NavLink></li>
+                    <li className={`p-1 ${styles.nav__link}`}><NavLink to='/mens'>Mens</NavLink></li>
+                    <li className={`p-1 ${styles.nav__link}`}><NavLink to='womens'>Womens</NavLink></li>
+                    <li className={`p-1 ${styles.nav__link}`}><NavLink to='basket'>Basket</NavLink></li>
                 </ul>
+                <div className='col-xs-3 flex justify-content-center align-items-aligncenter'>
+                    test text
+                </div>
             </nav>
         </header>
     )
