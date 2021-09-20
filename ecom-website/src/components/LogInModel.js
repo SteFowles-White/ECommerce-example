ad095f080c444b058f511653fdb34c6c8df23f51
SteFowles-White <fowleswhite@gmail.com>
2021-09-20 22:41:20 +0100

    added styles to the login model

--- ecom-website/src/components/LogInModel.js
+++ ecom-website/src/components/LogInModel.js
@@ -1,36 +1,49 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import style from "./loginModel.module.css";
 
 const LogInModel = () => {
-    return (
+  return (
+    <div
+      className={`${style.overlay} flex justify-content-center align-items-aligncenter`}
+    >
+      <section data-testid="login-model" className={`${style.login__form} p-3`}>
+        <form>
+          <fieldset
+            className={`${style.login__form__fieldset} flex justify-content-center flex-direction-rowcolumn`}
+          >
+            <legend className={`${style.login__form__legend} pb-3`}>
+              Log in
+            </legend>
+            <div className="flex justify-content-center flex-direction-rowcolumn pb-2 text-center">
+              <label className="pb-1">User Name</label>
+              <input className="col-sm-12 p-1" type="text" />
+            </div>
+            <div className="flex justify-content-center flex-direction-rowcolumn pb-2 text-center">
+              <label className="pb-1">Password</label>
+              <input className="col-sm-12 p-1" type="password" />
+            </div>
+          </fieldset>
+        </form>
         <div>
-            <section data-testid="login-model">
-                <form>
-                    <div>
-                        <label>Some text
-                        </label>
-                        <input type="text" />
-                    </div>
-                </form>
-                <div>
-                    <button>Submit</button>
-                </div>
-            </section>
+          <button>Submit</button>
         </div>
-
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 const RenderLogInModel = () => {
-    console.log(document.getElementById('login-model'))
-    return (
-        <React.Fragment>
-            { ReactDOM.createPortal( <LogInModel />, document.getElementById('login-model'))}
-        </React.Fragment>
-    )
-}
-
-
-
+  console.log(document.getElementById("login-model"));
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <LogInModel />,
+        document.getElementById("login-model")
+      )}
+    </React.Fragment>
+  );
+};
 
 export default RenderLogInModel;
